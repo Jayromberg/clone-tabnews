1e296fbab68ec2e94235118ca1b359254b14d103
jayromberg <jayro.dev@gmail.com>
2023-12-03 19:43:01 -0300

    feat: update `database`

--- infra/database.js
+++ infra/database.js
@@ -1,7 +1,13 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client();
+  const client = new Client({
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+  });
   await client.connect();
   const result = await client.query(queryObject);
   await client.end();
