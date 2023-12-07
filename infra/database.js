8e24e2dd6616b93694e0e1fe8e83abfcf29a494d
jayromberg <jayro.dev@gmail.com>
2023-12-07 16:03:53 -0300

    feat: add `jsconfig.json` and `.env`

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    user: process.env.PGUSER,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
   const result = await client.query(queryObject);
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from "../../../../infra/database";
+import database from "infra/database";
 
 async function status(_request, response) {
   const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result);
+  console.log(result.rows);
   response.status(200).json({ status: "ok" });
 }
 
